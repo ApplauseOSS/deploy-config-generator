f16397d8f131282510cacc0a415d57baef6f5a2a
Andrew Gaffney <agaffney@applause.com>
2019-01-28 12:54:46 -0600

    PD-4033 Perform variable replacements after YAML is parsed
    This commit also adds implicit type conversion based on the stated
    field type. A similar type conversion was previously done by the YAML
    parser, but that was more of a side effect.

--- deploy_config_generator/vars.py
+++ deploy_config_generator/vars.py
@@ -1,9 +1,19 @@
 import re
 import shlex
+import six
 
 from deploy_config_generator.errors import VarsParseError, VarsReplacementError
 
 BASE_VAR_END_TOKENS = ('\r', '\n', ' ')
+
+# Common regex for var name
+# Must start with letter/underscore and contain only letter/number/underscore
+RE_VAR_NAME = r'([A-Za-z_][A-Za-z0-9_]*)'
+
+ESCAPE_SEQUENCES = {
+    r'\"': '"',
+    r'\n': '\n',
+}
 
 
 class Vars(dict):
@@ -45,6 +55,9 @@ class Vars(dict):
                                 lineno = lexer.lineno - 1
                             raise VarsParseError("Did not find expected token '=' after var name", path=path, line=lineno)
                         try:
+                            # Process escape sequences
+                            var_value = self.replace_escape_sequences(var_value)
+                            # Replace vars
                             self[var_name] = self.replace_vars(var_value, allow_var_references=allow_var_references)
                         except Exception as e:
                             raise VarsParseError(str(e), path=path, line=(lexer.lineno - 1))
@@ -68,6 +81,17 @@ class Vars(dict):
                 else:
                     var_value += token
 
+    def replace_escape_sequences(self, value):
+        def replace_escape(match):
+            if match.group(0) in ESCAPE_SEQUENCES:
+                return ESCAPE_SEQUENCES[match.group(0)]
+            return match.group(0)
+
+        ret = value
+        ret = re.sub(r'\\.', replace_escape, ret)
+
+        return ret
+
     def replace_vars(self, value, allow_var_references=True):
         def replace_var(match):
             if not allow_var_references:
@@ -77,13 +101,21 @@ class Vars(dict):
             except KeyError:
                 raise VarsReplacementError("Unknown variable '%s'" % match.group(1))
 
-        ret = value
-        # Common regex for var name
-        # Must start with letter/underscore and contain only letter/number/underscore
-        re_var_name = r'([A-Za-z_][A-Za-z0-9_]+)'
-        # Replace bracketed vars
-        ret = re.sub(r'\$\{%s\}' % (re_var_name), replace_var, ret)
-        # Replace non-bracketed vars
-        ret = re.sub(r'\$%s' % (re_var_name), replace_var, ret)
+        if isinstance(value, list):
+            ret = []
+            for item in value:
+                ret.append(self.replace_vars(item))
+        elif isinstance(value, dict):
+            ret = {}
+            for item in value:
+                ret[item] = self.replace_vars(value[item])
+        elif isinstance(value, six.string_types):
+            ret = value
+            # Replace bracketed vars
+            ret = re.sub(r'\$\{%s\}' % RE_VAR_NAME, replace_var, ret)
+            # Replace non-bracketed vars
+            ret = re.sub(r'\$%s' % RE_VAR_NAME, replace_var, ret)
+        else:
+            ret = value
 
         return ret
--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ class IntegrationTests(Command):
 
 setup(
     name='applause-deploy-config-generator',
-    version='0.5.4',
+    version='0.6.0',
     url='https://github.com/ApplauseAQI/applause-deploy-config-generator',
     license='Applause',
     description='Utility to generate service deploy configurations',
